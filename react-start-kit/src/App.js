5046903d123047b2f806c84dd23b39b4524c76e1
cameronstlaurent <77070934+cameronstlaurent@users.noreply.github.com>
2022-02-01 11:47:48 -0700

    a

--- react-start-kit/src/App.js
+++ react-start-kit/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+// function ====> Component JSX(markup/data)
+//components **** must start from a single node
+// element
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>nav</nav>
+    <header>
+      <h1>React Starter Kit</h1>
+      <p>something</p>
+    </header>
+    </>
   );
 }
 
